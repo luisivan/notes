2a48ea810e601405adb44377b8010378a180ac00
Luis Iván <me@luisivan.net>
2014-06-17 11:42:58 +0200

    Adding a Promises polyfill for 1.1 and 1.3 Minimal UI Relying on activities to save blobs

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,27 +7,27 @@ module.exports = function (grunt) {
   grunt.initConfig({
 
     clean: {
-			hooks: ['.git/hooks/pre-commit']
-		},
+      hooks: ['.git/hooks/pre-commit']
+    },
 
-		shell: {
+    shell: {
       hooks: {
         command: 'cp git-hooks/pre-commit .git/hooks/'
       }
-		},
+    },
 
-		jshint: {
-			all: ['Gruntfile.js', 'app/js/*.js'],
-			options: {
-				jshintrc: '.jshintrc'
-			}
-		}
+    jshint: {
+      all: ['Gruntfile.js', 'app/js/*.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-shell');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.task.registerTask('hookmeup', ['clean:hooks', 'shell:hooks']);
   grunt.task.registerTask('linter', ['jshint']);
--- app/js/Recorder.js
+++ app/js/Recorder.js
@@ -5,49 +5,88 @@
 
   var m = window.m;
 
-  var recorder = {};
-
   var activityRequest = null;
+
+  var recorder = {};
 
   recorder.controller = function() {
     this.recording = m.prop(false);
-    this.blob = this.blobURL = this.text = m.prop('');
+    this.blob = m.prop(null);
+    this.blobURL = m.prop('');
 
     this.record = function() {
 
       if (!this.recording()) {
 
-        m.startComputation();
-        var _this = this;
-        Mic.record(function(err, blob) {
-          if (blob) {
-            _this.blob(blob);
-            _this.blobURL(URL.createObjectURL(blob));
-          }
-          m.endComputation();
-        });
+        this.recording(true);
+        // Let's make Mithril run to its endComputation
+        // before running our own
+        setTimeout(function() {  
+          m.startComputation();
+          Mic.record().then(function(blob) {
+            this.blob(blob);
+            URL.revokeObjectURL(this.blobURL());
+            this.blobURL(URL.createObjectURL(blob));
+            m.endComputation();
+          }.bind(this), function() {
+            this.recording(false);
+            m.endComputation();
+          }.bind(this));
+        }, 1);
       } else {
         Mic.stop();
+        this.recording(false);
       }
-
-      this.recording(!this.recording());
 
     }.bind(this);
 
-    this.postResult = function() {
-      this.text('This will return the processed text outta the audio');
-      activityRequest.postResult(this.text());
+    this.save = function() {
+
+      if (activityRequest) {
+        activityRequest.postResult(this.blob());
+        return;
+      }
+
+      new window.MozActivity({
+        name: 'open',
+        data: {
+          type: ['audio/ogg'],
+          blob: this.blob(),
+          allowSave: true
+        }
+      });
+
+    }.bind(this);
+
+    this.share = function() {
+
+      new window.MozActivity({
+        name: 'share',
+        data: {
+          blob: this.blob()
+        }
+      });
+
     }.bind(this);
   };
 
   recorder.view = function(ctrl) {
-    return m('div', [
-      m('button', {
-        class: ctrl.recording(),
-        onclick: ctrl.record
-      }, ctrl.recording()),
-      m('audio', { autoplay: true, controls: true, src: ctrl.blobURL() }),
-      m('button', { onclick: ctrl.postResult }, 'OK')
+    return m('section.skin-dark[role="region"]', [
+      m('header', [
+        m('button.action-icon.email-share', {
+          disabled: !ctrl.blobURL(), onclick: ctrl.share }),
+        m('menu[type="toolbar"]', [
+          m('button', { disabled: !ctrl.blobURL(), onclick: ctrl.save }, 'Save')
+        ]),
+        m('h1', 'Recorder')
+      ]),
+      m('section#record', [
+        m('div#mic', {
+          class: ctrl.recording(),
+          onclick: ctrl.record
+        }),
+        m('audio', { autoplay: true, controls: true, src: ctrl.blobURL() })
+      ])
     ]);
   };
 
@@ -55,7 +94,6 @@
 
   navigator.mozSetMessageHandler('activity', function(activityRequest) {
 
-    console.log(activityRequest);
     activityRequest = activityRequest;
 
   });
